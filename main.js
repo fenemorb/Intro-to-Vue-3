26397e4015f789f2c174751230cec4f3dd6b64d3
Bryn <fenemorb@gmail.com>
2023-01-05 17:30:55 +1100

    solved challenge

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      description: "100% cashmere wool. Locally made in Naarm by hand.",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://www.google.com/",
+    };
+  },
+});
